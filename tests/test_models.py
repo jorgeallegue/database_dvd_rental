d7869c5c5ef48b1ffa86bf785b6dfbddca551efa
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-19 19:54:47 +0200

    test: Update tests to reflect correct director assignment logic in Pelicula class

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,4 @@
-from models.main_model import Director, DAO_CSV_Director
+from models.main_model import Director, DAO_CSV_Director, Pelicula
 
 def test_create_director():
     director = Director("Robert Redford")
@@ -12,3 +12,35 @@ def test_dao_directores_traer_findAll():
 
     assert len(directores) == 8
     assert directores[7] == Director("Charlie Chaplin", 8)
+
+def test_create_pelicula():
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", 9)
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula._id_director == 9
+    assert pelicula.id == -1
+    assert pelicula.director is None
+
+def test_create_pelicula_and_informar_director_completo():
+    director = Director("Peter Jackson", 9)
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", director)
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula._id_director == 9
+    assert pelicula.id == -1
+    assert pelicula.director == director
+
+def test_asigna_director_a_pelicula():
+    pelicula = Pelicula("El señor de los anillos", "Sauron es muy malo", -1)
+
+    director = Director("peter Jackson", 9)
+
+    pelicula.director = director
+
+    assert pelicula.titulo == "El señor de los anillos"
+    assert pelicula.sinopsis == "Sauron es muy malo"
+    assert pelicula.id == -1
+    assert pelicula.director == director
+    assert pelicula._id_director == 9
