e450b3bc6fd0cbeed5af22870741a7b673e19073
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-19 17:06:24 +0200

    feat: Add Director class and DAO_CSV_Director implementation - Implemented the Director class with initialization, representation, and equality methods. - Added an abstract DAO class with abstract methods for common CRUD operations. - Created DAO_CSV_Director class that implements the findAll method to read directors from a CSV file. - Ensured the Director class correctly checks for equality based on name and ID.

--- models/main_model.py
+++ models/main_model.py
@@ -1,9 +1,18 @@
 from abc import ABC, abstractmethod
+import csv
 
 class Director:
     def __init__(self, nombre: str, id: int = -1):
         self.nombre = nombre
         self.id = id
+    
+    def __repr__(self) -> str:
+        return f"Director ({self.id}): {self.nombre}"
+    
+    def __eq__(self, other:object) -> bool:
+        if isinstance(other,Director):
+            return self.nombre == other.nombre and self.id == other.id
+        return False
 
 class DAO(ABC):
     """
@@ -28,4 +37,13 @@ class DAO(ABC):
         pass
         
 class DAO_CSV_Director(DAO):
-    
+    def __init__(self, path):
+        self.path = path
+
+    def findAll(self):
+        with open(self.path, "r", newline="") as fichero:
+            lector_csv = csv.DictReader(fichero, delimiter=";", quotechar="´")
+            lista = []
+            for registro in lector_csv:
+                lista.append(Director(registro["nombre"], int(registro["id"])))
+        return lista
