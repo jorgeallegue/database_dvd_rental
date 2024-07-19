37b3d99d164a9dd6da6223509dbb93e9a35f06b3
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-19 15:58:32 +0200

    feat: Added DAO abstract class and DAO_CSV_Director class

--- models/main_model.py
+++ models/main_model.py
@@ -1,4 +1,31 @@
+from abc import ABC, abstractmethod
+
 class Director:
     def __init__(self, nombre: str, id: int = -1):
         self.nombre = nombre
         self.id = id
+
+class DAO(ABC):
+    """
+    @abstractmethod
+    def create(self, instancia):
+        pass
+    
+    @abstractmethod
+    def update(self, instancia):
+        pass
+    
+    @abstractmethod
+    def delete(self, id: int):
+        pass
+    
+    @abstractmethod
+    def read(self, id: int):
+        pass
+    """
+    @abstractmethod
+    def findAll(self):
+        pass
+        
+class DAO_CSV_Director(DAO):
+    
