57a8a2e4d851bc864057d3c27523da521c6949db
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-19 17:04:09 +0200

    test: Added tests for DAO_CSV_Director findAll method

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,14 @@
-from models.main_model import Director
+from models.main_model import Director, DAO_CSV_Director
 
 def test_create_director():
     director = Director("Robert Redford")
 
     assert director.nombre == "Robert Redford"
     assert director.id == -1
+
+def test_dao_directores_traer_findAll():
+    dao = DAO_CSV_Director("tests/data/directores.csv")
+    directores = dao.findAll()
+
+    assert len(directores) == 8
+    assert directores[7] == Director("Charlie Chaplin", 8)
